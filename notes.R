60c74f6f0ca9f4254573d9cb01c4172ba6c210a7
fhui28 <fhui28@gmail.com>
2022-04-03 10:51:40 +1000

    working on nonzeromean_BFcoefs and starting option for unstructured (full rank) Sigma/G matrices

--- notes.R
+++ notes.R
@@ -5,7 +5,7 @@
 ## 1. Consider moving calls of other packages from :: to using importFrom
 ## 2. Delete all Rd files in man (but not figures!), the NAMESPACE file, and any compiled C++ files
 ## 3. Check package (Ctrl+Shift+E)
-## 4. Build (optional)
+##4. Build (optional)
 ## 5. Push to github
 
 #Removed from DESCRIPTION since we now turn off compilation of the TMB C++ files [learning from how James Thorson does it with VAST!]
@@ -17,13 +17,113 @@ LinkingTo:
 ##--------------------------------------
 ## Random testing jazz
 ##-------------------------------------
+library(autoFRK)
+library(FRK)
+library(MASS)
+library(mvabund)
+library(mvtnorm)
+library(ROCR)
+library(sp)
+library(RandomFields)
+library(tidyverse)
+
+##------------------------------
+## **Example 1a: Fitting a CBFM to spatial multivariate presence-absence data**
+## simulated from a spatial latent variable model
+## Please note the data generation process (thus) differs from CBFM.
+##------------------------------
+set.seed(2021)
+num_sites <- 1000 # 500 (units) sites for training set + 500 sites for testing.
+num_spp <- 50 # Number of species
+num_X <- 4 # Number of regression slopes
+
+#spp_slopes <- matrix(runif(num_spp * num_X, -1, 1), nrow = num_spp)
+spp_slopes <- cbind(rnorm(num_spp, -0.5, sd = 0.25), rnorm(num_spp, 0.5, sd = 0.25), rnorm(num_spp, -0.25, sd = 0.1), rnorm(num_spp, 0.25, sd = 0.1))
+spp_intercepts <- runif(num_spp, -2, 0)
+
+# Simulate spatial coordinates and environmental covariate components
+# We will use this information in later examples as well
+xy <- data.frame(x = runif(num_sites, 0, 5), y = runif(num_sites, 0, 5))
+X <- rmvnorm(num_sites, mean = rep(0,4))
+colnames(X) <- c("temp", "depth", "chla", "O2")
+dat <- data.frame(xy, X)
+mm <- model.matrix(~ temp + depth + chla + O2 - 1, data = dat) %>%
+scale %>%
+as.matrix
+
+# Simulate latent variable component
+# We will use this information in later examples as well
+true_lvs <- RFsimulate(model = RMexp(var=1, scale=2),
+x = xy$x, y = xy$y, n = 2)@data %>%
+as.matrix
+spp_loadings <- matrix(runif(num_spp * 2, -1, 1), nrow = num_spp)
+set.seed(NULL)
+
+# Simulate spatial multivariate abundance data (presence-absence)
+# We will use this information in later examples as well
+eta <- tcrossprod(cbind(1,mm), cbind(spp_intercepts,spp_slopes)) +
+tcrossprod(true_lvs, spp_loadings)
+simy <- matrix(rbinom(num_sites * num_spp, size = 1,
+prob = plogis(eta)), nrow = num_sites)
+
+# Form training and test sets
+dat_train <- dat[1:500,]
+dat_test <- dat[501:1000,]
+simy_train <- simy[1:500,]
+simy_test <- simy[501:1000,]
+rm(X, mm, spp_loadings, true_lvs, xy, simy, dat)
+
+
+# Fit stacked GLM as a baseline
+fitstacked <- manyglm(simy_train ~ temp + depth + chla + O2, family = binomial(), data = dat_train)
+
+
+# Set up spatial basis functions for CBFM -- Most users will start here!
+# We will also use this basis functions in some later examples
+num_basisfunctions <- 25 # Number of spatial basis functions to use
+# Training set basis functions
+train_basisfunctions <- mrts(dat_train[,c("x","y")], num_basisfunctions) %>%
+as.matrix %>%
+{.[,-(1)]} # Remove the first intercept column
+# Testing set basis functions
+test_basisfunctions <- mrts(dat_train[,c("x","y")], num_basisfunctions) %>%
+predict(newx = dat_test[,c("x","y")]) %>%
+as.matrix %>%
+{.[,-c(1)]}
+
+
+
+# Fit CBFM
+mm <- model.matrix(~ temp + depth + chla + O2, data = dat_train)[,-1,drop=FALSE]
+useformula <- ~ 1
+fitcbfm <- CBFM(y = simy_train, formula_X = useformula, data = dat_train,
+B_space = train_basisfunctions, B_time = mm,
+family = binomial(), 
+control = list(trace = 1, nonzeromean_B_time = TRUE),
+Sigma_control = list(rank = c(5,1))
+)
+
+
+fitcbfm_pure <- CBFM(y = simy_train, formula_X = ~ temp + depth + chla + O2, 
+                     data = dat_train,
+                     B_space = train_basisfunctions, 
+                     family = binomial(), control = list(trace = 1))
+
+
+
+ggmatplot(spp_slopes, fitcbfm_pure$betas[,-1]) + geom_abline(intercept = 0, slope = 1)
+ggmatplot(spp_slopes, fitcbfm$basis_effects_mat[,fitcbfm$num_B_space+(1:fitcbfm$num_B_time)]) + geom_abline(intercept = 0, slope = 1)
+
+
+
 
 y = simy_train
 useformula <- ~ 1
+mm <- model.matrix(~ temp + depth + chla + O2, data = dat_train)[,-1,drop=FALSE]
 formula_X = useformula
 data = dat_train
-B_space = train_basisfunctions
 family =  binomial()
+B_space = train_basisfunctions
 B_time = mm
 B_spacetime = NULL
 offset = NULL
@@ -35,9 +135,9 @@ stderrors = TRUE
 select = FALSE
 start_params = list(betas = NULL, basis_effects_mat = NULL, dispparam = NULL, powerparam = NULL, zeroinfl_prob = NULL)
 TMB_directories = list(cpp = system.file("executables", package = "CBFM"), compile = system.file("executables", package = "CBFM"))
-control = list(maxit = 100, convergence_type = "parameters", tol = 1e-4, seed = NULL, trace = 1, ridge = 0)
-Sigma_control = list(rank = c(2,1), maxit = 100, tol = 1e-4, method = "LA", trace = 0)
-G_control = list(rank = 5, nugget_profile = seq(0.05, 0.95, by = 0.05), maxit = 100, tol = 1e-4, method = "LA", trace = 0)
+control = list(maxit = 100, convergence_type = "parameters", tol = 1e-4, seed = NULL, trace = 1, ridge = 0, nonzeromean_B_time = TRUE)
+Sigma_control = list(rank = c(5,1), maxit = 100, tol = 1e-4, method = "LA", trace = 0)
+G_control = list(rank = c(5,5), nugget_profile = seq(0.05, 0.95, by = 0.05), maxit = 100, tol = 1e-4, method = "LA", trace = 0)
 k_check_control = list(subsample = 5000, n.rep = 400)
 
 
@@ -129,36 +229,3 @@ k_check_control = list(subsample = 5000, n.rep = 400)
 
 
 ##---------------------
-
-useformula <- ~ temp + depth + chla + O2
-fitcbfm_nb <- CBFM(y = simy_count$y, formula_X = useformula, data = dat, B_space = basisfunctions, family = nb2(), control = list(trace = 1))
-
-fitcbfm_ztnb <- CBFM(y = simy_count$y, formula_X = useformula, data = dat, B_space = basisfunctions, family = ztnb2(), control = list(trace = 1))
-
-
-
-
-par(mfrow = c(1,2))
-plot(spp_slopes_ztnb, fitcbfm_nb$betas[,2:5])
-abline(0,1)
-plot(spp_slopes_ztnb, fitcbfm_ztnb$betas[,2:5])
-abline(0,1)
-
-plot(fitcbfm_ztnb)
-
-AIC(fitcbfm_nb, use_edf = TRUE)
-AIC(fitcbfm_ztnb, use_edf = TRUE)
-
-par(mfrow = c(1,2))
-plot(simy_count$linear_predictor, fitcbfm_nb$linear_predictor)
-abline(0,1)
-plot(simy_count$linear_predictor, fitcbfm_ztnb$linear_predictor)
-abline(0,1)
-
-
-
-
-#-----------------------------
-#-----------------------------
-
-
