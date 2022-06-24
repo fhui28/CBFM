f04738d2d773eeba311529f587fa1e2fdccef59a
fhui28 <fhui28@gmail.com>
2022-06-24 14:29:41 +1000

    Updating and simplifying neghessfamily calculations a bit

--- R/neghessfamily.R
+++ R/neghessfamily.R
@@ -4,10 +4,10 @@
 
 .neghessfamily <- function(family, eta, y, phi = NULL, powerparam = NULL, zieta = NULL, trial_size, return_matrix = FALSE, 
                            domore = FALSE, tol = 1e-6) {
-        if(family$family[1] %in% c("Beta")) {
-                out <- grad(.sbetalogit, x = eta, y = y, phi = phi)    
-                }
-        if(family$family[1] %in% c("binomial")) {
+     if(family$family[1] %in% c("Beta")) {
+          out <- grad(.sbetalogit, x = eta, y = y, phi = phi)    
+          }
+     if(family$family[1] %in% c("binomial")) {
 #           if(family$link == "probit") {
 #                mu <- binomial(link = "probit")$linkinv(eta)
 # 
@@ -23,52 +23,52 @@
                         out <- trial_size*binomial()$var(mu)
                         }
                 }
-        if(family$family[1] %in% c("Gamma")) {
+     if(family$family[1] %in% c("Gamma")) {
                 mu <- Gamma(link = "log")$linkinv(eta)
                 out <- (1/phi) * y / mu
                 }
-        if(family$family[1] %in% c("gaussian")) {
+     if(family$family[1] %in% c("gaussian")) {
                 out <- 1/phi
                 }
-        if(family$family[1] == "negative.binomial") {
+     if(family$family[1] == "negative.binomial") {
                 mu <- exp(eta)
                 out <- (phi*y + 1) * mu / (1 + phi*mu)^2               
                 }
-        if(family$family[1] %in% c("poisson")) {
+     if(family$family[1] %in% c("poisson")) {
                 out <- exp(eta) 
                 }
-        if(family$family[1] == "ztpoisson") {
+     if(family$family[1] == "ztpoisson") {
                 out <- .hess_ztpoisson(eta = eta, y = y)
                 # lambda <- exp(eta)
                 # out <- exp(eta) + exp(eta)/(exp(lambda)-1) + exp(2*eta) * exp(lambda) / (exp(lambda)-1)^2
                 # rm(lambda)
                 }
-        if(family$family[1] == "ztnegative.binomial") {
+     if(family$family[1] == "ztnegative.binomial") {
                 out <- .hess_ztnbinom(eta = eta, y = y, size = 1/phi)
                 }
-        if(family$family[1] %in% c("tweedie")) {
+     if(family$family[1] %in% c("tweedie")) {
              two_minus_powerparam <- 2-powerparam
              exp_two_minus_powerparam_linpred <- exp(two_minus_powerparam * eta)
              exp_one_minus_powerparam_linpred <- exp((two_minus_powerparam-1) * eta)
              out <-  1/phi * (two_minus_powerparam*exp_two_minus_powerparam_linpred - y*(two_minus_powerparam-1)*exp_one_minus_powerparam_linpred)    
              }
-        if(family$family[1] %in% c("zipoisson")) {
-             lambda <- exp(eta)
-             rhat <- numeric(length(y))
-             rhat[y == 0] <- as.vector(plogis(zieta + lambda))[y == 0]
-             out <- lambda * (1-rhat) * (1-lambda*rhat)
+     if(family$family[1] %in% c("zipoisson")) {
+          lambda <- exp(eta)
+          rhat <- numeric(length(y))
+          rhat[y == 0] <- as.vector(plogis(zieta + lambda))[y == 0]
+          out <- lambda * (1-rhat) * (1-lambda*rhat)
          
-             if(domore) {
-                  # out is already the collection of weights for betasbetas and basiseffectsbasiseffects. So we need the other terms involving the zero-inflation component...
+          if(domore) {
+               # out is already the collection of weights for betasbetas and basiseffectsbasiseffects. So we need the other terms involving the zero-inflation component...
 
-                  dhat <- exp(zieta) / (exp(zieta) + exp(-lambda))
-                  phat <- plogis(zieta)
-                  out_zeroinflzeroinfl <- phat * (1-phat) - ((y == 0) * 1) * dhat * (1-dhat)
-                  out_zeroinflzeroinfl[out_zeroinflzeroinfl < 0] <- 0 ## At the moment, needed primarily for zero-inflated and zero-truncated models where weights can be negative (by design?!)
+               dhat <- exp(zieta+lambda) / (exp(zieta+lambda) + 1)
+               phat <- plogis(zieta)
+               out_zeroinflzeroinfl <- phat * (1-phat) - ((y == 0) * 1) * dhat * (1-dhat)
+               out_zeroinflzeroinfl[out_zeroinflzeroinfl < 0] <- 0 ## At the moment, needed primarily for zero-inflated and zero-truncated models where weights can be negative (by design?!)
                   
-                  out_zeroinflbetas <- -(exp(zieta) * lambda * exp(-lambda)) / (exp(zieta) + exp(-lambda))^2
-                  out_zeroinflbetas[y > 0] <- 0
-                  }
+               out_zeroinflbetas <- -(exp(zieta+lambda) * lambda) / (exp(zieta+lambda) + 1)^2
+               out_zeroinflbetas[y > 0] <- 0
+               }
              }
         if(family$family[1] %in% c("zinegative.binomial")) { 
              lambda <- exp(eta)
@@ -87,26 +87,25 @@
                      out_zeroinflzeroinfl <- phat * (1-phat) - ((y == 0) * 1) * dhat
                      out_zeroinflzeroinfl[out_zeroinflzeroinfl < 0] <- 0 ## At the moment, needed primarily for zero-inflated and zero-truncated models where weights can be negative (by design?!)
                      
-                     out_zeroinflbetas <- -(exp(zieta)*lambda*(1+phi*lambda)^(-1/phi-1)) / (exp(zieta) + (1+phi*lambda)^(-1/phi))^2
+                     out_zeroinflbetas <- -(exp(zieta+lambda) * lambda) / (exp(zieta+lambda) + 1)^2
                      out_zeroinflbetas[y > 0] <- 0
-                     }
              }
-
-        out[out < 0] <- 0 ## At the moment, needed primarily for zero-inflated and zero-truncated models where weights can be negative (by design?!)
-        out[!is.finite(out)] <- 0 ## Needed when for zero-truncated models where weights on the very rare occasional be stupid?
-
-        if(!domore)
-                return(as.vector(out))
-        if(domore) {
-                if(!(family$family[1] %in% c("zipoisson","zinegative.binomial")))
-                        return(list(out = as.vector(out)))
-                if(family$family[1] %in% c("zipoisson","zinegative.binomial"))
-                        return(list(out = as.vector(out), 
-                                    out_zeroinflzeroinfl = out_zeroinflzeroinfl, # matrix of the same dimension as y
-                                    out_zeroinflbetas = out_zeroinflbetas # matrix of the same dimension as y)
-                                    )) 
-                }
         }
-     
-     
+
+     out[out < 0] <- 0 ## At the moment, needed primarily for zero-inflated and zero-truncated models where weights can be negative (by design?!)
+     out[!is.finite(out)] <- 0 ## Needed when for zero-truncated models where weights on the very rare occasional be stupid?
+     if(!domore)
+          return(as.vector(out))
+     if(domore) {
+          if(!(family$family[1] %in% c("zipoisson","zinegative.binomial")))
+               return(list(out = as.vector(out)))
+          if(family$family[1] %in% c("zipoisson","zinegative.binomial"))
+               return(list(out = as.vector(out), 
+                           out_zeroinflzeroinfl = out_zeroinflzeroinfl, # matrix of the same dimension as y
+                           out_zeroinflbetas = out_zeroinflbetas # matrix of the same dimension as y)
+               )) 
+          }
+     }
+
+
 
