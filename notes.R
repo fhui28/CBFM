eaaa9f288fb2b2aada2d662f218ae2e53c812fa2
fhui28 <fhui28@gmail.com>
2023-07-03 16:48:27 +1000

    Modifying notes to test CBFM with tensor product penalties

--- notes.R
+++ notes.R
@@ -2,7 +2,6 @@
 
 ## GENERAL APPROACH TO MODIFYING PACKAGE
 ## 1. Do all changes
-## 1. Consider moving calls of other packages from :: to using importFrom
 ## 2. Delete all Rd files in man (but not figures!), the NAMESPACE file, and any compiled C++ files
 ## 3. Check package
 ##4. Build (optional)
@@ -15,7 +14,7 @@ LinkingTo:
 
 
 ##--------------------------------------
-## Random testing jazz
+#' # Random testing jazz
 ##-------------------------------------
 library(autoFRK)
 library(FRK)
@@ -33,12 +32,12 @@ registerDoParallel(cores = detectCores()-2)
 
 
 ##------------------------------
-## **Example 0: Fitting a CBFM to data from a spatial CBFM
-## Estimate betas and basis functions alright, but as expected (although it's worse than I thought) estimation of G and Sigma is pretty poor. It maybe something to do with estimating the scale issue when they are estimated separately
+#' # **Example 1: Fitting a CBFM to data from a spatial CBFM**
+#' This is a just a general testing function
 ##------------------------------
-set.seed(2022)
+set.seed(072023)
 num_sites <- 500 # 500 (units) sites 
-num_spp <- 50 # Number of species
+num_spp <- 10 # Number of species
 num_X <- 4 # Number of regression slopes
  
 spp_slopes <- matrix(runif(num_spp * num_X, -1, 1), nrow = num_spp)
@@ -67,43 +66,70 @@ simy <- create_CBFM_life(family = binomial(), formula = useformula, data = dat,
                          Sigma = list(space = true_Sigma_space), G = list(space = true_G_space))
 
 
-# Fit CBFMs
+#' ## Fit different flavors of CBFMs
 fitcbfm_fixed <- CBFM(y = simy$y, formula = ~ s(temp) + depth + chla + O2, data = dat,
                    B_space = basisfunctions, family = binomial(), 
                    control = list(trace = 1), G_control = list(rank = 5), Sigma_control = list(rank = 5))
 
-fake_gam <- gam(simy$y[,1] ~ s(temp), data = dat)
-MM_temp <- model.matrix(fake_gam)[,-1]
-Sigma_temp <- .pinv(fake_gam$smooth[[1]]$S[[1]])
 
+
+fake_gam <- gam(simy$y[,1] ~ s(temp), data = dat, family = binomial(), method = "REML", fit = FALSE)
+MM_temp <- fake_gam$X[,-1]
+Sigma_temp <- .pinv(fake_gam$smooth[[1]]$S[[1]])
 fitcbfm_random <- CBFM(y = simy$y, formula = ~ depth + chla + O2, data = dat,
                    B_space = basisfunctions, B_time = MM_temp, family = binomial(), 
-                   control = list(trace = 1, initial_betas_dampen = 1), 
-                   G_control = list(rank = c(5,"full")), 
+                   control = list(trace = 1, inner_maxit = 10),
+                   G_control = list(rank = c(5,"full"), structure = c("unstructured", "homogeneous")), 
                    Sigma_control = list(rank = c(5,1), custom_time = Sigma_temp))
 
 
-ggmatplot(fitcbfm_fixed$betas[,-c(1:4)], fitcbfm_random$basis_effects_mat[,-c(1:24)], shape = 19) + 
-     geom_abline(intercept = 0, slope = 1) +
-     scale_color_viridis_d()
+Sigma_temp
+fitcbfm_random$Sigma_time
+fitcbfm_random$Sigma_time/Sigma_temp
+fitcbfm_random$G_time
+
+matplot(simy$linear_predictors, fitcbfm_fixed$linear_predictors, pch = 19); abline(0,1)
+matplot(simy$linear_predictors, fitcbfm_random$linear_predictors, pch = 19); abline(0,1)
 
 
 
-ggmatplot(cbind(spp_slopes,spp_gear), fitcbfm_fixed$betas[,-1]) + geom_abline(intercept = 0, slope = 1)
-ggmatplot(cbind(spp_slopes,spp_gear), cbind(fitcbfm_random$betas[,-1], fitcbfm_random$basis_effects_mat[,1])) + geom_abline(intercept = 0, slope = 1)
+fake_gam <- gam(simy$y[,1] ~ te(temp, depth), data = dat, family = binomial(), method = "REML", fit = FALSE)
+MM_tensorprod <- fake_gam$X[,-1]
+Sigma_tensorprod <- lapply(1:length(fake_gam$smooth[[1]]$S), function(x) .pinv(fake_gam$smooth[[1]]$S[[x]]))
+fitcbfm_tensorprod <- CBFM(y = simy$y, formula = ~ chla + O2, data = dat,
+                       B_space = basisfunctions, B_spacetime = MM_tensorprod, family = binomial(), 
+                       control = list(trace = 1),
+                       G_control = list(rank = c(5,2), structure = c("unstructured", "homogeneous")), 
+                       Sigma_control = list(rank = c(5,1), custom_spacetime = Sigma_tensorprod))
 
 
-ggmatplot(simy$basis_effects_mat, fitcbfm_sp$basis_effects_mat, shape = 1) + geom_abline(intercept = 0, slope = 1)
-ggmatplot(true_Sigma_space[lower.tri(true_Sigma_space)], fitcbfm_sp$Sigma_space[lower.tri(fitcbfm_sp$Sigma_space)]) + geom_abline(intercept = 0, slope = 1)
-ggmatplot(true_G_space[lower.tri(true_G_space)], fitcbfm_sp$G_space[lower.tri(fitcbfm_sp$G_space)]) + geom_abline(intercept = 0, slope = 1)
+fitcbfm_random$basis_effects_mat
+Sigma_tensorprod
+fitcbfm_tensorprod$Sigma_spacetime
+fitcbfm_tensorprod$G_spacetime
+
+matplot(simy$linear_predictors, fitcbfm_fixed$linear_predictors, pch = 19); abline(0,1)
+matplot(simy$linear_predictors, fitcbfm_tensorprod$linear_predictors, pch = 19); abline(0,1)
+
+
+
+
+fitcbfm_singlespp <- CBFM(y = simy$y[,1,drop=FALSE], formula = ~ chla + O2, data = dat,
+                       B_space = basisfunctions, B_spacetime = MM_tensorprod, family = binomial(), 
+                       control = list(trace = 1),
+                       G_control = list(rank = c(5,"full"), structure = c("unstructured", "homogeneous")), 
+                       Sigma_control = list(rank = c(5,1), custom_spacetime = Sigma_tensorprod))
+
+fitcbfm_singlespp$basis_effects_mat
+fitcbfm_singlespp$Sigma_spacetime
+
 
 
 
 ##------------------------------
-## **Example 2 modified: Fitting a CBFM to spatial multivariate presence-absence data**
-## simulated from a spatial latent variable model
-## Please note the data generation process (thus) differs from CBFM.
-## This example was constructed to test the G_control$structure = "identity" argument, which is like fitting a hierarchical GAM
+#' # **Example 2 modified: Fitting a CBFM to spatial multivariate presence-absence data simulated from a spatial latent variable model**
+#' Please note the data generation process (thus) differs from CBFM.
+#' This example was constructed to test the G_control$structure = "identity" argument, which is like fitting a hierarchical GAM
 ##------------------------------
 set.seed(082022)
 num_sites <- 1000 # 500 (units) sites for training set + 500 sites for testing.
@@ -140,7 +166,7 @@ dat_long <- data.frame(simy_train, dat_train) %>%
      pivot_longer(spp1:spp20, names_to = "species") %>% 
      mutate(species = fct_inorder(species))
 
-hgam <- mgcv::gam(value ~ s(temp, by = species, bs = "gp", id = 1), data = dat_long, family = "binomial", method = "REML")
+hgam <- gam(value ~ s(temp, by = species, bs = "gp", id = 1), data = dat_long, family = "binomial", method = "REML")
 #hgam_select <- mgcv::gam(value ~ s(temp, by = species, id = 1), data = dat_long, select = TRUE, family = "binomial", method = "REML")
 #hgam_fs <- mgcv::gam(value ~ s(temp, species, bs = "fs"), data = dat_long, family = "binomial", method = "REML")
 
@@ -209,7 +235,7 @@ data.frame(temp = dat_train$temp, cbfm = fitcbfm$linear_predictors, hgam = matri
 
 
 ##----------------------------------
-## Custom testing
+#' Custom testing
 ##----------------------------------
 function() {
      y = simy_train
@@ -242,18 +268,16 @@ function() {
 
 
 function() {
-     sel_spp <- c(2,36,70:74)
-     y = fit1$y[,sel_spp,drop=FALSE]
-     formula = ~ SVVESSEL + GLO_SURFTEMP_month + GLO_BOTTEMP_month + logSTRESS_Q95_YR
-     # formula ~ s(SVVESSEL, bs="ts", m=1) + s(GLO_SURFTEMP_month, bs="ts", m=1) + s(GLO_BOTTEMP_month, bs="ts", m=1) + s(logSTRESS_Q95_YR, bs="ts", m=1)
+     y = simy$y[,1,drop=FALSE]
+     formula = ~ chla + O2
+     data = dat
      ziformula <- NULL
-     data = fit1$data
-     B_space = B_space_FRK 
-     family = ztnb2()
+     B_space = basisfunctions
+     B_time = NULL
+     B_spacetime = MM_tensorprod
+     family = binomial() 
      ncores = 8
      control = list(trace = 1)
-     B_time = NULL
-     B_spacetime = NULL
      offset = NULL
      gamma = 1
      zigamma = 1
@@ -267,25 +291,25 @@ function() {
      ziselect = FALSE
      start_params = list(betas = NULL, zibetas = NULL, basis_effects_mat = NULL, dispparam = NULL, powerparam = NULL)
      TMB_directories = list(cpp = system.file("executables", package = "CBFM"), compile = system.file("executables", package = "CBFM"))
-     G_control = list(rank = 2)
-     Sigma_control = list(rank = 2)
+     G_control = list(rank = c(5,"full"), structure = c("unstructured", "homogeneous"))
+     Sigma_control = list(rank = c(5,1), custom_spacetime = Sigma_tensorprod)
      k_check_control = list(subsample = 5000, n.rep = 400)
      }
 
 
+
 function() {
-     Ginv = new_LoadingnuggetG_space$covinv
-     basis_effects_mat = centered_BF_mat
-     Sigmainv = new_LoadingnuggetSigma_space$covinv
-     B = B_space
+     Sigmainv = new_LoadingnuggetSigma_spacetime$invcov
+     basis_effects_mat = centered_BF_mat 
+     Ginv = new_LoadingnuggetG_spacetime$invcov
+     B = B_spacetime
      X = X
      ziX = ziX
-     y_vec = as.vector(y) 
+     y_vec = as.vector(y)
      linpred_vec = c(new_fit_CBFM_ptest$linear_predictors)
-     dispparam = new_fit_CBFM_ptest$dispparam 
+     dispparam = new_fit_CBFM_ptest$dispparam
      powerparam = new_fit_CBFM_ptest$powerparam
      zibetas = new_fit_CBFM_ptest$zibetas 
-     trial_size = trial_size
-     use_rank_element = 1
-     return_correlation = is.null(Sigma_control$custom_space)     
+     estimate_lambda = estimate_lambda_not_Sigma
+     which_B = 3
      }
