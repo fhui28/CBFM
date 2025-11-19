3266d7a48d822cb1599cde40cf4f454e8700c6a5
fhui28 <fhui28@gmail.com>
2025-11-19 23:20:10 +1100

    fix bug in hidden gratia effects function

--- R/utils.R
+++ R/utils.R
@@ -27,7 +27,7 @@
                sel_rowcols <- sel_rowcols[-(1:num_ziX)]
                }
           nullfit$Vp <- as.matrix(object$covar_components$topleft[sel_rowcols, sel_rowcols, drop = FALSE])
-          out <- suppressMessages(parametric_effects(object = nullfit))
+          out <- suppressMessages(parametric_effects(object = nullfit, data = nulldat))
           out$species <- colnames(object$y)[j]
           }
      
@@ -72,7 +72,7 @@
                sel_rowcols <- sel_rowcols[-(1:num_ziX)]
                }
           nullfit$Vp <- nullfit$Ve <- nullfit$Vc <- as.matrix(object$covar_components$topleft[sel_rowcols, sel_rowcols,drop=FALSE])
-          out <- suppressMessages(smooth_estimates(object = nullfit))
+          out <- suppressMessages(smooth_estimates(object = nullfit, data = nulldata))
           out$species <- colnames(object$y)[j]
           }
      
